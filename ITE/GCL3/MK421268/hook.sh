3c877f491830cf7938779d5ea5418e76c369cc0d
777lahcim <polish.agent007@gmail.com>
2026-03-06 18:08:45 +0100

    Test

--- ITE/GCL3/MK421268/hook.sh
+++ ITE/GCL3/MK421268/hook.sh
@@ -3,6 +3,6 @@
 commit_msg=$(cat "$1")
 
 if [[ ! $commit_msg =~ "MK421268" ]]; then
-  echo "Error: prefix JK417545 nedded in commit"
+  echo "Error: prefix MK421268 nedded in commit"
   exit 1
 fi
