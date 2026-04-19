3439d72f54f072668f5cc8e8d53902a92ffd2b24
Bogdomeble <bartbod4@gmail.com>
2026-04-19 18:54:10 +0000

    BB419678 more pipeline tweaks

--- ITE/GCL1/BB419678/Dockerfiles_lab6/workflow.groovy
+++ ITE/GCL1/BB419678/Dockerfiles_lab6/workflow.groovy
@@ -26,30 +26,36 @@ pipeline {
             steps {
                 dir('ITE/GCL1/BB419678/Dockerfiles_lab6') {
                     sh 'docker build -t neovim-tester-jenkins -f Dockerfile.nvim.test .'
-                    // Przywrócone || true - błędy systemd nie zatrzymają pipeline'u
                     sh 'docker run --rm neovim-tester-jenkins || true'
                 }
             }
         }
 
-        stage('Archiwizacja Artefaktów (Paczka DEB)') {
+stage('Archiwizacja Artefaktów (Paczka DEB)') {
             steps {
                 dir('ITE/GCL1/BB419678/Dockerfiles_lab6') {
                     sh '''
-                        # Tworzymy tymczasowy kontener, żeby wyciągnąć plik
+                        # Tworzymy tymczasowy kontener (nie musi działać)
                         docker create --name temp-archive-container neovim-builder-jenkins
                         
-                        # Znajdujemy wygenerowany plik .deb (nazwa zależy od wersji nvim)
-                        DEB_NAME=$(docker exec temp-archive-container ls build | grep .deb | head -n 1)
+                        # Kopiujemy cały folder build z kontenera do obecnego katalogu roboczego
+                        docker cp temp-archive-container:/workspace/build ./temp_build_dir
                         
-                        # Kopiujemy go do workspace jenkinsa
-                        docker cp temp-archive-container:/workspace/build/$DEB_NAME ./neovim-final.deb
+                        # Szukamy pliku .deb w skopiowanym folderze i zmieniamy mu nazwę na docelową
+                        # Używamy find, żeby złapać plik niezależnie od dokładnej wersji neovima
+                        cp ./temp_build_dir/*.deb ./neovim-final.deb
                         
+
+                        echo "--- SPRAWDZANIE ZALEŻNOŚCI PACZKI ---"
+                        # metadane pliku .deb
+                        dpkg -I ./neovim-final.deb | grep 'Depends'
+
+                        # Sprzątamy
                         docker rm temp-archive-container
+                        rm -rf ./temp_build_dir
                     '''
                     
-                    // ARCHIVE ARTIFACTS - to jest pomarańczowa strzałka z Twojego rysunku
-                    // Paczka .deb zawiera w sobie definicje zależności (np. libvterm / libtermkey)
+                    // Teraz plik neovim-final.deb istnieje w workspace
                     archiveArtifacts artifacts: 'neovim-final.deb', fingerprint: true
                 }
             }
