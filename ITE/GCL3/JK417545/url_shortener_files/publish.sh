e7d62f959d3b3719e55b1b733cc3fc505c1ddb5b
Jakub <jakub.konecki9@gmail.com>
2026-04-27 13:06:36 +0200

    JK417545 update plikow do pipeline

--- ITE/GCL3/JK417545/url_shortener_files/publish.sh
+++ ITE/GCL3/JK417545/url_shortener_files/publish.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
 BUILD_IMAGE=$1
 
 echo "------------------------------------------"
-echo "Starting Publish process..."
+echo "Starting Publish to NPM.js process..."
 
 docker run --rm -v $(pwd):/out $BUILD_IMAGE cp -r /app/package.json /app/dist /out/
-docker run --rm -v $(pwd):/out -w /out node:20-alpine npm pack
 
-echo "Package created successfully."
+docker run --rm -v $(pwd):/out -w /out node:20-alpine sh -c "
+  echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
+  npm pack
+  npm publish --access public
+"
+
 echo "------------------------------------------"
